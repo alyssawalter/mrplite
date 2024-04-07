c35b0991b56bc3907b2a01d28e415c2a6c82af74
Alyssa Walter <alyssamwalter@lewisu.edu>
2024-04-07 15:34:19 -0500

    created item group forms

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Item
+from .models import Item, ItemGroup
 
 
 class ItemForm(forms.ModelForm):
@@ -14,3 +14,15 @@ class EditItemForm(forms.ModelForm):
         model = Item
         fields = ['item_sku', 'item_name', 'item_group', 'unit_of_measurement',
                   'reorder_point', 'selling_price', 'manufactured', 'min_qty_for_manufacturing']
+
+
+class ItemGroupForm(forms.ModelForm):
+    class Meta:
+        model = ItemGroup
+        fields = ['item_group_name']
+
+
+class EditItemGroupForm(forms.ModelForm):
+    class Meta:
+        model = ItemGroup
+        fields = ['item_group_name']
