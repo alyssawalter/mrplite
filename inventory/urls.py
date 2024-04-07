64f2d52035137f5a762c13f86ddaf68c73678eb6
Alyssa Walter <alyssamwalter@lewisu.edu>
2024-04-07 17:28:13 -0500

    Updated views to use Generic views

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,10 +4,10 @@ from .views import *
 
 app_name = "inventory"
 urlpatterns = [
-    path("", views.items_list, name="items_list"),
-    path("item/<str:item_sku>/", views.item_detail, name="item_detail"),
-    path("<str:item_sku>/update", views.edit_item, name="edit_item"),
-    path("create_item/", views.create_item, name="create_item"),
+    path('items/', ItemListView.as_view(), name='item_list'),
+    path('items/create/', ItemCreateView.as_view(), name='create_item'),
+    path('items/<str:pk>/', ItemDetailView.as_view(), name='item_detail'),
+    path('items/<str:pk>/edit/', ItemUpdateView.as_view(), name='edit_item'),
     path("item_groups/", ItemGroupListView.as_view(), name="item_group_list"),
     path("item_groups/create/", CreateItemGroupView.as_view(), name="create_item_group"),
     path("item_groups/<int:pk>/", ItemGroupDetailView.as_view(), name="item_group_detail"),
--- inventory/views.py
+++ inventory/views.py
@@ -6,43 +6,39 @@ from .forms import ItemForm, EditItemForm, EditItemGroupForm, ItemGroupForm
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
-def items_list(request):
-    items = Item.objects.order_by('item_sku')
-    return render(request, 'inventory/item_list.html', {'items': items})
+class ItemListView(ListView):
+    model = Item
+    template_name = 'inventory/item_list.html'
+    context_object_name = 'items'
+    ordering = ['item_sku']
 
 
-def item_detail(request, item_sku):
-    try:
-        # Retrieve the item object with the given SKU from the database
-        item = Item.objects.get(item_sku=item_sku)
-    except Item.DoesNotExist:
-        raise Http404("Item does not exist")
-    # Pass the item object to the template for rendering
-    return render(request, 'inventory/item_detail.html', {'item': item})
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'inventory/item_detail.html'
+    context_object_name = 'item'
 
 
-def create_item(request):
-    if request.method == 'POST':
-        form = ItemForm(request.POST)
-        if form.is_valid():
-            new_item = form.save() # Save the form data and get the newly created item instance
-            # Redirect to the detail page of the new item
-            return HttpResponseRedirect(reverse("inventory:item_detail", args=(new_item.item_sku,)))
-    else:
-        form = ItemForm()
-    return render(request, 'inventory/create_item.html', {'form': form})
+class ItemCreateView(CreateView):
+    model = Item
+    template_name = 'inventory/create_item.html'
+    form_class = ItemForm
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('inventory:item_detail', kwargs={'pk': self.object.item_sku})
 
 
-def edit_item(request, item_sku):
-    item = get_object_or_404(Item, item_sku=item_sku)
-    if request.method == 'POST':
-        form = EditItemForm(request.POST, instance=item)
-        if form.is_valid():
-            form.save()
-            return redirect('inventory:item_detail', item_sku=item_sku)
-    else:
-        form = EditItemForm(instance=item)
-    return render(request, 'inventory/edit_item.html', {'form': form, 'item': item})
+class ItemUpdateView(UpdateView):
+    model = Item
+    template_name = 'inventory/edit_item.html'
+    form_class = EditItemForm
+    context_object_name = 'item'
+
+    def get_success_url(self):
+        return reverse_lazy('inventory:item_detail', kwargs={'pk': self.object.item_sku})
 
 
 class ItemGroupListView(ListView):
